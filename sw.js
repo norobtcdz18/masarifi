8aa2adec6596ef1d08315433a487ea4a9a4dca64
napp <norobtc1@gmail.com>
2026-06-25 21:43:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,20 @@
 const CACHE_NAME = 'masarifi-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // إذا وجدنا الملف في الكاش، نرجعه، وإلا نطلبه من الشبكة
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
